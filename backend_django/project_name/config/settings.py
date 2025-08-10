f51ef55cd5d71d8d1e7b57b95d0b8a1be251e406
abhishek-0x49310 <101920127+abhishek-0x49310@users.noreply.github.com>
2025-08-10 18:12:36 +0530

    Refactor Django project structure and add custom startapp command
    Reorganized the Django backend project by consolidating files from 'minimalistic' and 'minimal-with-admin' into a unified 'backend_django' structure. Updated settings.py to adjust BASE_DIR and INSTALLED_APPS. Added a custom management command 'startapp_' for creating new apps from a remote template. Removed legacy and redundant files from previous project layouts.

--- backend_django/project_name/config/settings.py
+++ backend_django/project_name/config/settings.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-f!c%=i-vy646_c@4cd6p24p!tdfu@hnjy1-g^z#)nes_#o485_'
@@ -12,7 +12,9 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
-INSTALLED_APPS = []
+INSTALLED_APPS = [
+    '{{ project_name }}'
+]
 
 MIDDLEWARE = []
 
